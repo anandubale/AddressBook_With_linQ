36fa25fc2b0431777c97db371aa4492f0e19b537
anandubale <au26199@gmail.com>
2022-02-06 11:36:45 +0530

    Created DataBase

---
+++ AddressBookTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace ConsoleApp1
+{
+    class AddressBookTable
+    {
+        public void CreateAddressBookDataTable()
+        {
+            //DataTable 
+            DataTable dataTable = new DataTable();
+        }
+    }
+}
